aba6e7ea2f3d7942196dd917e4029568728367ea
destiny <destinyomorhienrhien1993@gmail.com>
2023-09-10 16:14:03 +0100

    more commit

--- endpointapp/views.py
+++ endpointapp/views.py
@@ -17,7 +17,7 @@ def ApiOverview(request):
         items = Endpoint.objects.filter(**request.query_params.dict())
        
     else:
-        items = Endpoint.objects.all()
+        items = Endpoint.objects.using('db.sqlite3').all()
  
     # if there is something in items else raise error
     if items:
